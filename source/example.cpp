ab76b9f954297b51171d0507ee44a5c3909ad2a7
Trofim <dualdual657@gmail.com>
2025-01-14 21:30:53 +1000

    Вынос функции todo_move_me_out в отдельный хидер

---
+++ source/example.cpp
@@ -0,0 +1,7 @@
+#include "example.hpp"
+#include <iostream>
+
+void example_function()
+{
+    std::cout << "Hello from example function!" << std::endl;
+}
---
+++ source/hello_world.cpp
@@ -0,0 +1,13 @@
+#include "example.hpp"
+#include "move_me_out.hpp"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+
+    example_function();
+    todo_move_me_out();
+
+    return 0;
+}
